c69648378d77d6f6b828042125be26625ac63cc6
Joffre21 <90282920+Joffre21@users.noreply.github.com>
2022-10-03 16:22:27 -0500

    Crear Dataframe con los correos
    Usar <pip install pandas>

--- MailReader.py
+++ MailReader.py
@@ -1,43 +1,64 @@
+#Librerías necesarias
 import imaplib
 import email
+import re
 import traceback
+import pandas as pd
+import base64
+import quopri
 
-mail_direction = "joffregvz00@gmail.com"
-password = "ljirhmuztysissru"
+#Función para descodificar textos
+def encoded_words_to_text(texto, bool):
+    if bool:    
+        nombre = texto.split("\"")
+        encoded_words = nombre[1]
+    else:
+        encoded_words = texto
+    try:
+        encoded_word_regex = r'=\?{1}(.+)\?{1}([B|Q])\?{1}(.+)\?{1}='
+        charset, encoding, encoded_text = re.match(encoded_word_regex, encoded_words).groups()
+        if encoding == 'B':
+            byte_string = base64.b64decode(encoded_text)
+        elif encoding == 'Q':
+            byte_string = quopri.decodestring(encoded_text)
+        if bool:
+            return "\""+ byte_string.decode(charset) + "\"" + nombre[2]
+        else:
+            return byte_string.decode(charset)
+    except:
+        return texto
 
-imap_server = "imap.gmail.com"
-port = 993
+#Lectura de correos
+def ExtraccionCorreos(mail_direction, password, imap_server, port):
+    try:
+        mail = imaplib.IMAP4_SSL(imap_server, port)
+        mail.login(mail_direction, password)
+        _, selected_mails = mail.select("[Gmail]/Spam")
+        #Seleccionar a los correos 'UNSEEN'
+        _, selected_mails = mail.search(None, 'SEEN')
+        for num in selected_mails[0].split():
+            _, data = mail.fetch(num , '(RFC822)')
+            _, bytes_data = data[0]
+            #Convertir desde Byte data al mensaje de correo
+            email_message = email.message_from_bytes(bytes_data)
+            for part in email_message.walk():
+                if part.get_content_type()=="text/plain" or part.get_content_type()=="text/html":
+                    message = part.get_payload(decode=True)
+                    break
+            lista_filas.append([encoded_words_to_text(email_message["subject"], False), encoded_words_to_text(email_message["from"], True), str(message.decode()), len(str(message.decode())), 1])
+            #Cerrado de conexión
+        mail.close()
+        mail.logout()
+    except Exception as e:
+        traceback.print_exc()
+        print(str(e))
 
-try:
-    mail = imaplib.IMAP4_SSL(imap_server, port)
-    mail.login(mail_direction, password)
-    mail.select("[Gmail]/Spam")
-    #mail.select("INBOX")
-    #select specific mails
-    _, selected_mails = mail.search(None, 'SEEN')
-    #total number of mails from specific user
-    print("Total Messages: " , len(selected_mails[0].split()))
-    for num in selected_mails[0].split():
-        _, data = mail.fetch(num , '(RFC822)')
-        _, bytes_data = data[0]
-        #convert the byte data to message
-        email_message = email.message_from_bytes(bytes_data)
-        print("\n===========================================")
+#Variables para generar el dataset
+lista_filas = []
 
-        #access data
-        print("Subject: ",email_message["subject"])
-        print("To:", email_message["to"])
-        print("From: ",email_message["from"])
-        print("Date: ",email_message["date"])
-        for part in email_message.walk():
-            if part.get_content_type()=="text/plain" or part.get_content_type()=="text/html":
-                message = part.get_payload(decode=True)
-                print("Message: \n", message.decode())
-                print("==========================================\n")
-                break
-        # close the connection and logout
-    mail.close()
-    mail.logout()
-except Exception as e:
-    traceback.print_exc()
-    print(str(e))
+ExtraccionCorreos("joffregvz00@gmail.com", "ljirhmuztysissru", "imap.gmail.com", 993)
+
+#Creación del Dataframe
+df = pd.DataFrame(lista_filas, columns=['Asunto', 'Emisor', 'Mensaje', 'Longitud', 'Spam'])
+print(df.head(20))
+print(df.size)
